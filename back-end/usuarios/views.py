cda0f1d8346a3f329aa482fdd4adf8b2781eb461
Allan Sergio <106421236+shutxx@users.noreply.github.com>
2024-04-29 22:58:33 -0300

    login/logout

--- back-end/usuarios/views.py
+++ back-end/usuarios/views.py
@@ -1,7 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
-from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 from rest_framework import generics
 from .serializers import TokenSerializer, UsuarioSerializer
