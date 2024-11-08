57e3f653f67ea3e89c7f31693be8c7280d79edd2
Lucas Sicuto <74109392+Lucassicuto@users.noreply.github.com>
2024-11-08 17:18:28 -0300

    add relatorio de vendas

--- back-end/vendas/urls.py
+++ back-end/vendas/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import VendaListAPIView, VendaCreateAPIView, VendaRetrieveAPIView, VendaDestroyAPIView, VendaUpdateAPIView
+from .views import VendaListAPIView, VendaCreateAPIView, VendaRetrieveAPIView, VendaDestroyAPIView, VendaUpdateAPIView, RelatorioView
 
 urlpatterns = [
     path('vendas/', VendaListAPIView.as_view(), name='vendas-list'),
     path('venda/create/', VendaCreateAPIView.as_view(), name='venda-create'),
     path('venda/detail/<int:pk>', VendaRetrieveAPIView.as_view(), name='venda-detail'),
     path('venda/delete/<int:pk>', VendaDestroyAPIView.as_view(), name='venda-delete'),
-    path('venda/update/<int:pk>', VendaUpdateAPIView.as_view(), name='venda-update')
+    path('venda/update/<int:pk>', VendaUpdateAPIView.as_view(), name='venda-update'),
+    path('venda/relatorio/', RelatorioView.as_view(), name='venda-relatorio')
 ]
--- back-end/vendas/views.py
+++ back-end/vendas/views.py
@@ -1,7 +1,12 @@
 from rest_framework import generics
 from .models import Venda
 from .serializers import VendaSerializer
+from rest_framework.views import APIView
 from rest_framework.pagination import PageNumberPagination
+from xhtml2pdf import pisa
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+
 
 class VendaListAPIView(generics.ListAPIView):
     queryset = Venda.objects.all()
@@ -23,3 +28,12 @@ class VendaDestroyAPIView(generics.DestroyAPIView):
 class VendaUpdateAPIView(generics.UpdateAPIView):
     queryset = Venda.objects.all()
     serializer_class = VendaSerializer
+
+class RelatorioView(APIView):
+    def get(self, request, *args, **kwargs):
+        vendas = Venda.objects.all()
+        html_content = render_to_string('relatorio.html', {'vendas': vendas})
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = 'attachment; filename="relatorio.pdf"'
+        pisa.CreatePDF(html_content, dest=response)
+        return response
