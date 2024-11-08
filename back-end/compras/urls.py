d1d886ffa71651b36c7a2eb9577906b47cf5bcef
Allan Sergio <106421236+shutxx@users.noreply.github.com>
2024-11-08 01:42:51 -0300

    add search compra att collection postman

--- back-end/compras/urls.py
+++ back-end/compras/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import CompraListAPIView, CompraCreateAPIView, CompraRetrieveAPIView, CompraUpdateAPIView, CompraDestroyAPIView
+from .views import CompraListAPIView, CompraCreateAPIView, CompraRetrieveAPIView, CompraUpdateAPIView, CompraDestroyAPIView, CompraSearch
 
 urlpatterns = [
     path('compras/', CompraListAPIView.as_view(), name='compras-list'),
     path('compra/create/', CompraCreateAPIView.as_view(), name='compra-create'),
     path('compra/detail/<int:pk>', CompraRetrieveAPIView.as_view(), name='compra-detail'),
     path('compra/delete/<int:pk>', CompraDestroyAPIView.as_view(), name='compra-delete'),
-    path('compra/update/<int:pk>', CompraUpdateAPIView.as_view(), name='compra-update')
+    path('compra/update/<int:pk>', CompraUpdateAPIView.as_view(), name='compra-update'),
+    path('compra/search/', CompraSearch.as_view(), name='compra-search')
 ]
--- back-end/compras/views.py
+++ back-end/compras/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics
+from rest_framework.views import APIView
+from django.db.models import Q
 from .models import Compra
 from .serializers import CompraSerializer
 from rest_framework.pagination import PageNumberPagination
@@ -23,3 +25,23 @@ class CompraDestroyAPIView(generics.DestroyAPIView):
 class CompraUpdateAPIView(generics.UpdateAPIView):
     queryset = Compra.objects.all()
     serializer_class = CompraSerializer
+
+class CompraSearch(APIView):
+    def get(self, request, *args, **kwargs):
+        query = request.query_params.get('query', None) 
+
+        if query:
+            compraSeach = Compra.objects.filter(
+                Q(IdFornecedor__NomeFantasia__icontains=query) | Q(IdFornecedor__CNPJ__icontains=query)
+            )
+        else:
+            compraSeach = Compra.objects.all()
+
+        pagination_class = PageNumberPagination()
+        pagination_class.page_size = 10
+
+        result_page = pagination_class.paginate_queryset(compraSeach, request)
+
+        serializer_class = CompraSerializer(result_page, many=True)
+
+        return pagination_class.get_paginated_response(serializer_class.data)
